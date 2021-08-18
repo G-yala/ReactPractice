7440a82914f0339405b0e4829f2ac1ea8d16ee43
Geronimo Ayala <vash4477@berkeley.edu>
2021-08-18 09:49:26 -0700

    Delete IdeaProjects directory

--- IdeaProjects/Book/src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] main) {
-        Book children = new Book("Baldy", "Captain Underpants", 96);
-
-        System.out.println(children.toString());
-    }
-}
